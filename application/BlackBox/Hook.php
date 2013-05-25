1e30096996f4eabfa79dde8389e4125f9d734b58
Diego Zanella <businessdad75@gmail.com>
2013-05-25 20:46:37 +0100

    Added check to hide BlackBox bar from non-Admin Users

--- application/BlackBox/Hook.php
+++ application/BlackBox/Hook.php
@@ -11,18 +11,23 @@
 
 class BlackBox_Hook
 {
-    public static function profiler()
-    {
-        if(func_get_arg(0) != BlackBox::DEBUG) {
-            return;
-        }
+		public static function profiler()
+		{
+				if(func_get_arg(0) != BlackBox::DEBUG) {
+						return;
+				}
 
-        BlackBox::getInstance()->getProfiler()->trace(func_get_arg(1));
-    }
+				BlackBox::getInstance()->getProfiler()->trace(func_get_arg(1));
+		}
 
-    public static function footer()
-    {
-        apply_filters('debug', 'Profiler Stopped');
-        include_once BLACKBOX_DIR."/application/debug-bar.php";
-    }
+		public static function footer()
+		{
+			// Don't display the bar to non-Admins
+			if(!current_user_can('manage_options')) {
+				return;
+			}
+
+			apply_filters('debug', 'Profiler Stopped');
+			include_once BLACKBOX_DIR."/application/debug-bar.php";
+		}
 }
